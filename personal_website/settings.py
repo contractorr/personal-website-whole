88bf20a93af21da3e00cf49d0a70a810a2482af2
contractorr <rajcontractor@hotmail.co.uk>
2021-01-18 11:39:44 +0000

    Removing secret key

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7_23k7s$0944x&gl+8#qxzm(i8pgdqanv(!zpw1_69ygkue4xd'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
